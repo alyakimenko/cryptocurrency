0a0be84bea822a46cf746cf701b80718961662d7
Mikhail Klementev <blame@dumpstack.io>
2020-01-18 10:42:35 +0000

    GitHub actions: tests

--- shell.nix
+++ shell.nix
@@ -1,5 +1,11 @@
 { pkgs ? import <nixpkgs> {} }:
 
 with pkgs; mkShell {
-  buildInputs = [ which go electrum ];
+  SSL_CERT_FILE = "${cacert}/etc/ssl/certs/ca-bundle.crt";
+  GIT_SSL_CAINFO = "${cacert}/etc/ssl/certs/ca-bundle.crt";
+
+  GO111MODULE = "on";
+  GOPATH = "/tmp/lib-cryptocurrency-nix-shell-pure-gopath";
+
+  buildInputs = [ git which go electrum ];
 }
