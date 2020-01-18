ee65f20d11a4ee1d679aba3256074eb7c4b449dd
Mikhail Klementev <blame@dumpstack.io>
2020-01-18 21:33:56 +0000

    Implements API call utxoSumForAddresses

--- cardano/webapi.go
+++ cardano/webapi.go
@@ -5,8 +5,10 @@
 package cardano
 
 import (
+	"bytes"
 	"encoding/json"
 	"net/http"
+	"strconv"
 )
 
 func endpoint() (url string) {
@@ -28,5 +30,47 @@ func status() (ok bool, err error) {
 	}
 
 	ok = result.IsServerOK
+	return
+}
+
+func utxoSumForAddresses(addresses []string) (sum int64, err error) {
+	payload := struct {
+		Addresses []string `json:"addresses"`
+	}{Addresses: addresses}
+
+	payloadBytes, err := json.Marshal(payload)
+	if err != nil {
+		return
+	}
+	body := bytes.NewReader(payloadBytes)
+
+	req, err := http.NewRequest("POST", endpoint()+"txs/utxoSumForAddresses", body)
+	if err != nil {
+		return
+	}
+	req.Header.Set("Content-Type", "application/json;charset=UTF-8")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+
+	var result struct{ Sum string }
+	err = json.NewDecoder(resp.Body).Decode(&result)
+	if err != nil {
+		return
+	}
+
+	if result.Sum == "" {
+		// not error, just zero
+		return
+	}
+
+	// >>> ada = 45_000_000_000
+	// >>> lovelace = 1_000_000
+	// >>> math.log2(ada * lovelace)
+	// 55.32077451964011
+	sum, err = strconv.ParseInt(result.Sum, 10, 64)
 	return
 }
--- cardano/webapi_test.go
+++ cardano/webapi_test.go
@@ -4,7 +4,9 @@
 
 package cardano
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestStatus(t *testing.T) {
 	ok, err := status()
@@ -14,5 +16,36 @@ func TestStatus(t *testing.T) {
 
 	if !ok {
 		t.Fatal("server is dead")
+	}
+}
+
+func Test_utxoSumForAddresses(t *testing.T) {
+	addresses := []string{
+		"Ae2tdPwUPEYz56j2qffuLfQEN9y4YuMbefJRqyfsHuuWX2TzRENMTX23pGr",
+		"Ae2tdPwUPEYwJGMJfPkC3bRn6c5oSGTwwuEWuR3ofDG5T6c6vH7XL5DMmGH",
+		"Ae2tdPwUPEZCdLWob8bJRXumWHNZ7rXWPjHGTq5qT9YeuaeXTicoNxFhmZt",
+		"Ae2tdPwUPEZ2SU9qv63oVGTLpPL1iPw6Xmm1xRT98KWvQpgYZBbj8gWfmJt",
+	}
+
+	sum, err := utxoSumForAddresses(addresses)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if sum != 32133700 {
+		t.Fatal("Wrong UTXO Sum")
+	}
+
+	addresses = []string{
+		"Ae2tdPwUPEZ6GQPHgXuM31aEFKm4ZHGULQfT2iSPDcqkMVbyDLfMhaoAaSd",
+	}
+
+	sum, err = utxoSumForAddresses(addresses)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if sum != 0 {
+		t.Fatal("Wrong UTXO Sum")
 	}
 }
